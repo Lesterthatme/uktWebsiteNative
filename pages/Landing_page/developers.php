3a3c19fc0ddd928f0c5d43124c6d3f5ef90eb2e5
lester merino <lester.merino21@gmail.com>
2026-02-19 13:29:24 +0800

    latest in the system

--- pages/Landing_page/developers.php
+++ pages/Landing_page/developers.php
@@ -1,70 +1,222 @@
+<div id="modalElement" class="fixed inset-0 bg-gray-600/50 flex items-end justify-center z-50 hidden">
+
+    <!-- Modal Box -->
+    <div class="bg-white w-[90vw] sm:w-[80vw] lg:w-[70vw] 
+                max-h-[90vh] rounded-xl shadow-lg overflow-hidden ">
+
+        <div class="p-6 overflow-y-auto max-h-[90vh]">
+
+            <!-- Title -->
+            <div class="text-center mb-8">
+                <h2 class="text-2xl sm:text-3xl font-bold text-green-700">
+                    BS Education Interns
+                </h2>
+                <p class="text-gray-500 mt-2 text-sm sm:text-base">
+                    Academic Year 2026
+                </p>
+            </div>
+
+            <!-- KEEP YOUR GRID HERE (no changes needed below this) -->
+            <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+
+                <!-- Intern Card -->
+                <div class="bg-white rounded-xl shadow-sm p-6 text-center">
+
+                    <!-- Image -->
+                    <div class="w-24 h-24 sm:w-28 sm:h-28 mx-auto mb-4">
+                        <img src="assets/images/developer/2026/educ3.jpg"
+                            class="w-full h-full object-cover rounded-full border-4 border-green-600">
+                    </div>
+
+                    <!-- Name -->
+                    <h3 class="text-base sm:text-lg font-semibold text-gray-800">
+                        Ellisha D. Valdesotto
+                    </h3>
+
+                    <!-- Major -->
+                    <p class="text-sm text-gray-500 mt-1">
+                        BS Education English
+                    </p>
+                </div>
+                <div class="bg-white rounded-xl shadow-sm p-6 text-center">
+
+                    <!-- Image -->
+                    <div class="w-24 h-24 sm:w-28 sm:h-28 mx-auto mb-4">
+                        <img src="assets/images/developer/2026/educ4.jpg"
+                            class="w-full h-full object-cover rounded-full border-4 border-green-600">
+                    </div>
+
+                    <!-- Name -->
+                    <h3 class="text-base sm:text-lg font-semibold text-gray-800">
+                        Charlotte E. Antonino
+                    </h3>
+
+                    <!-- Major -->
+                    <p class="text-sm text-gray-500 mt-1">
+                        BS Education English
+                    </p>
+                </div>
+                <div class="bg-white rounded-xl shadow-sm p-6 text-center">
+
+                    <!-- Image -->
+                    <div class="w-24 h-24 sm:w-28 sm:h-28 mx-auto mb-4">
+                        <img src="assets/images/developer/2026/educ1.jpg"
+                            class="w-full h-full object-cover rounded-full border-4 border-green-600">
+                    </div>
+
+                    <!-- Name -->
+                    <h3 class="text-base sm:text-lg font-semibold text-gray-800">
+                        Nikki G. Jorlucas
+                    </h3>
+
+                    <!-- Major -->
+                    <p class="text-sm text-gray-500 mt-1">
+                        BS Education English
+                    </p>
+                </div>
+                <div class="bg-white rounded-xl shadow-sm p-6 text-center">
+
+                    <!-- Image -->
+                    <div class="w-24 h-24 sm:w-28 sm:h-28 mx-auto mb-4">
+                        <img src="assets/images/developer/2026/educ2.jpg"
+                            class="w-full h-full object-cover rounded-full border-4 border-green-600">
+                    </div>
+
+                    <!-- Name -->
+                    <h3 class="text-base sm:text-lg font-semibold text-gray-800">
+                        Yasmine T. Soliman
+                    </h3>
+
+                    <!-- Major -->
+                    <p class="text-sm text-gray-500 mt-1">
+                        BS Education English
+                    </p>
+                </div>
+                <div class="bg-white rounded-xl shadow-sm p-6 text-center">
+
+                    <!-- Image -->
+                    <div class="w-24 h-24 sm:w-28 sm:h-28 mx-auto mb-4">
+                        <img src="assets/images/developer/2026/educ5.jpg"
+                            class="w-full h-full object-cover rounded-full border-4 border-green-600">
+                    </div>
+
+                    <!-- Name -->
+                    <h3 class="text-base sm:text-lg font-semibold text-gray-800">
+                        RM
+                    </h3>
+
+                    <!-- Major -->
+                    <p class="text-sm text-gray-500 mt-1">
+                        BS Education English
+                    </p>
+                </div>
+
+                <!-- Repeat for other interns -->
+
+                <!-- Adviser -->
+                <div class="bg-green-700 text-white rounded-xl p-6 text-center sm:col-span-2 lg:col-span-3">
+
+                    <div class="w-24 h-24 sm:w-28 sm:h-28 mx-auto mb-4">
+                        <img src="assets/images/developer/2026/advisor1.jpg"
+                            class="w-full h-full object-cover rounded-full border-4 border-white">
+                    </div>
+
+                    <h3 class="text-base sm:text-lg font-semibold">
+                        Ma'am Eiffel M. Marcelo
+                    </h3>
+
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
 <div class="developers-header">
     <img src="assets/images/developers_header.jpg" alt="Developers Header">
 </div>
 
+<div class="position-relative text-white"
+    style="
+        background-image: url('/ukt/assets/images/developer/2026/group1.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        padding: 60px 0;
+     ">
+    
+    <!-- Bootstrap Overlay -->
+    <div class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-75"></div>
 
+    <!-- Your Content -->
+    <div class="position-relative container">
 
-
-
-<div class="dev-title text-center my-5">
-    <p class="text-muted fs-7">IT Student of the Bulacan Agricultural State College from the Philippines.</p>
-</div>
-<div class="text-center py-4 my-5">
-    <h3 class="fw-bold text-success mb-3">2026 Intern</h3>
-    <hr class="w-25 mx-auto border-success border-3">
-</div>
-<div class="d-flex flex-wrap justify-content-center gap-4 my-5 container" data-aos="fade-up" data-aos-duration="2000">
-    <div class="Developer_card">
-        <div class="dev_header">
-            <img src="assets/images/developer/2026/student1.png" alt="Profile Picture" class="img-fluid object-fit-cover ">
+        <div class="text-center py-4 my-5" data-aos="fade-up" data-aos-duration="2000">
+            <h3 class="fw-bold mb-3 fs-1" style="color: white;">2026 Intern</h3>
+            <hr class="w-25 mx-auto border-light border-3">
         </div>
-        <div class="info">
-            <h3>Lester Arjay B. Merino</h3>
-            <p class="fw-bold">Developer</p>
-            <p>San Ildefonso Bulacan, Philippines</p>
-            <p>Bulacan Agricultural State College</p>
-            <div class="socials">
-                <a target="_blank" href="https://www.facebook.com/bikernaerot/"><i class="ri-facebook-box-fill"></i></a>
-                <a target="_blank" href="https://github.com/Lesterthatme"><i class="ri-github-fill"></i></a>
+
+        <div class="d-flex flex-wrap justify-content-center gap-4 my-5"
+            data-aos="fade-up" data-aos-duration="2000">
+
+            <div class="Developer_card boxShadow">
+                <div class="dev_header ">
+                    <img src="assets/images/developer/2026/intern3.JPG" alt="Profile Picture" class="img-fluid object-fit-cover ">
+                </div>
+                <div class="info">
+                    <h3>Lester Arjay B. Merino</h3>
+                    <p class="fw-bold">Developer</p>
+                    <p>San Ildefonso Bulacan, Philippines</p>
+                    <p>Bulacan Agricultural State College</p>
+                    <div class="socials">
+                        <a target="_blank" href="https://www.facebook.com/bikernaerot/"><i class="ri-facebook-box-fill"></i></a>
+                        <a target="_blank" href="https://github.com/Lesterthatme"><i class="ri-github-fill"></i></a>
+                    </div>
+                </div>
             </div>
+            <div class="Developer_card boxShadow">
+                <div class="dev_header">
+                    <img src="assets/images/developer/2026/intern2.JPG" alt="Profile Picture" class="img-fluid object-fit-cover ">
+                </div>
+                <div class="info">
+                    <h3>Jessa V. Baniador</h3>
+                    <p class="fw-bold">Data Quality Assurance & Administrator</p>
+                    <p>San Ildefonso Bulacan, Philippines</p>
+                    <p>Bulacan Agricultural State College</p>
+                    <div class="socials">
+                        <a target="_blank" href="https://www.facebook.com/Jssbndr"><i class="ri-facebook-box-fill"></i></a>
+                        <a target="_blank" href="https://github.com/Sseej"><i class="ri-github-fill"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="Developer_card boxShadow">
+                <div class="dev_header">
+                    <img src="assets/images/developer/2026/intern1.JPG" alt="Profile Picture" class="img-fluid object-fit-cover ">
+                </div>
+                <div class="info">
+                    <h3>Louie C. Jabonillo</h3>
+                    <p class="fw-bold">Multimedia</p>
+                    <p>San Ildefonso Bulacan, Philippines</p>
+                    <p>Bulacan Agricultural State College</p>
+                    <div class="socials">
+                        <a target="_blank" href="https://www.facebook.com/CoachWowie"><i class="ri-facebook-box-fill"></i></a>
+                        <a target="_blank" href="https://github.com/Louie1234567890"><i class="ri-github-fill"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div data-aos="fade-up" data-aos-duration="2000" class="d-flex justify-content-center cursor-pointer">
+            <a id="clickBtn" style="color: white;" class="fs-4">Pre-Service Teacher <i class="bi bi-box-arrow-up-right"></i></a>
         </div>
     </div>
-    <div class="Developer_card">
-        <div class="dev_header">
-            <img src="assets/images/developer/2026/student2.jpg" alt="Profile Picture" class="img-fluid object-fit-cover ">
-        </div>
-        <div class="info">
-            <h3>Jessa V. Baniador</h3>
-            <p class="fw-bold">Data Quality Assurance & Administrator</p>
-            <p>San Ildefonso Bulacan, Philippines</p>
-            <p>Bulacan Agricultural State College</p>
-            <div class="socials">
-                <a target="_blank" href="https://www.facebook.com/Jssbndr"><i class="ri-facebook-box-fill"></i></a>
-                <a target="_blank" href="https://github.com/Sseej"><i class="ri-github-fill"></i></a>
-            </div>
-        </div>
-    </div>
-    <div class="Developer_card">
-        <div class="dev_header">
-            <img src="assets/images/developer/2026/student3.jpg" alt="Profile Picture" class="img-fluid object-fit-cover ">
-        </div>
-        <div class="info">
-            <h3>Louie C. Jabonillo</h3>
-            <p class="fw-bold">Multimedia</p>
-            <p>San Ildefonso Bulacan, Philippines</p>
-            <p>Bulacan Agricultural State College</p>
-            <div class="socials">
-                <a target="_blank" href="https://www.facebook.com/CoachWowie"><i class="ri-facebook-box-fill"></i></a>
-                <a target="_blank" href="https://github.com/Louie1234567890"><i class="ri-github-fill"></i></a>
-            </div>
-        </div>
-    </div>
-
-
 </div>
 
-<div class="text-center py-4 my-5">
-    <h3 class="fw-bold text-success mb-3">2025 Intern</h3>
+<div class="text-center py-4 my-5" data-aos="fade-up" data-aos-duration="2000">
+    <h3 class="fw-bold text-success mb-3 fs-1">2025 Intern</h3>
     <hr class="w-25 mx-auto border-success border-3">
 </div>
 <div class="d-flex flex-wrap justify-content-center gap-4 my-5 container" data-aos="fade-up" data-aos-duration="2000">
@@ -149,8 +301,8 @@
         </div>
     </div>
 </div>
-<div class="text-center py-4 my-5">
-    <h3 class="fw-bold text-success mb-3">2024 Intern</h3>
+<div class="text-center py-4 my-5" data-aos="fade-up" data-aos-duration="2000">
+    <h3 class="fw-bold text-success mb-3 fs-1">2024 Intern</h3>
     <hr class="w-25 mx-auto border-success border-3">
 </div>
 <div class="d-flex flex-wrap justify-content-center gap-4 my-5 container" data-aos="fade-up" data-aos-duration="2000">
@@ -215,10 +367,4 @@
         </div>
     </div>
 
-</div>
-
-
-<div id="imageModal" class="image-modal">
-    <span class="developer-close-btn">&times;</span>
-    <img id="fullImage" alt="Full View">
 </div>
