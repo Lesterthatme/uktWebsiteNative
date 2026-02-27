6818be0e45158fa35de92d67c8a8bc456f465b10
lester merino <lester.merino21@gmail.com>
2026-02-27 09:57:56 +0800

    latest

--- function/php/auth/signup.php
+++ function/php/auth/signup.php
@@ -2,5 +2,83 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/ukt' . '/connection/dbconnection.php';
 
 if (isset($_POST['signupBtn'])) {
-    
+    $conn->begin_transaction();
+    try {
+        $first_name = $conn->real_escape_string($_POST['ap_firstname']);
+        $mi = $conn->real_escape_string($_POST['ap_mi']);
+        $last_name = $conn->real_escape_string($_POST['ap_lastname']);
+        $birthday = $conn->real_escape_string($_POST['birthday']);
+        $age = $conn->real_escape_string($_POST['age']);
+        $sex = $conn->real_escape_string($_POST['sex']);
+        $username = $conn->real_escape_string($_POST['username']);
+        $email = $conn->real_escape_string($_POST['email']);
+        $password = $conn->real_escape_string($_POST['password']);
+        $confirm_password = $conn->real_escape_string($_POST['confirm_password']);
+
+        // Check if email already exists
+        $email_query = "SELECT email FROM user_account WHERE email = '$email'";
+        $email_result = $conn->query($email_query);
+
+        if ($email_result->num_rows > 0) {
+            $_SESSION['alreadyExist_message'] = 'Email already exists!';
+            header("Location: ukt/pages/content_manager/signup.php");
+            exit;
+        }
+
+        // Check if passwords match
+        if ($password !== $confirm_password) {
+            $_SESSION['passNotMatch'] = 'Password and Confirm Password do not match!';
+            header("Location: ukt/pages/content_manager/signup.php");
+            exit;
+        }
+
+        // Handle image upload
+        $upload_folder = '../../assets/uploads/profile_pic/';
+        $default_image = "default-profile.jpeg";
+        $image = $_FILES['image']['name'];
+        $image_tmp = $_FILES['image']['tmp_name'];
+
+        if (!empty($image)) {
+            if (!file_exists($upload_folder)) {
+                mkdir($upload_folder, 0777, true); // Ensure folder exists
+            }
+            move_uploaded_file($image_tmp, $upload_folder . $image);
+            $final_image = $image;
+        } else {
+            $final_image = $default_image;
+        }
+
+        // Insert into user_account
+        $insert_user_account = "INSERT INTO user_account (username, email, password, image, user_type, account_status) 
+                            VALUES ('$username', '$email', '$password', '$final_image', 'Content manager', 'pending')";
+
+        if ($conn->query($insert_user_account)) {
+            $user_id = $conn->insert_id;
+            // Insert into authorized_person
+            $insert_authorized_person = "INSERT INTO authorized_person (ap_firstname, ap_mi, ap_lastname, birthday, age, sex, user_id) 
+                                     VALUES ('$first_name', '$mi', '$last_name', '$birthday', '$age', '$sex', '$user_id')";
+
+            if ($conn->query($insert_authorized_person)) {
+                $conn->commit();
+                echo "
+                <script>
+                alert('Signup successful!');
+            </script>";
+                header('Location: ../../../pages/content_manager/login.php');
+                exit;
+            } else {
+                $conn->rollback();
+                header('Location: ukt/pages/content_manager/signup.php');
+                exit;
+            }
+        } else {
+            $conn->rollback();
+            header('Location: ukt/pages/content_manager/signup.php');
+            exit;
+        }
+    } catch (Exception $e) {
+        $conn->rollback();
+        header('Location: ukt/pages/content_manager/signup.php');
+        exit;
+    }
 }
--- pages/content_manager/forgot_password_process.php
+++ pages/content_manager/forgot_password_process.php
@@ -35,73 +35,74 @@ if ($row = mysqli_fetch_assoc($result)) {
         }
     </style>
 </head>
+
 <body>
     <div class="login-container">
-      
+
         <!-- make this manageable also -->
-          <?php
-                    if (isset($_POST['reset'])) {
-                        $email = $_POST['email'];
-                    } else {
-                        exit();
+        <?php
+        if (isset($_POST['reset'])) {
+            $email = $_POST['email'];
+        } else {
+            exit();
+        }
+
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
+
+        require '../../mail/Exception.php';
+        require '../../mail/PHPMailer.php';
+        require '../../mail/SMTP.php';
+
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'uktkratie@gmail.com';                     // SMTP username
+            $mail->Password   = 'fgww ccwv zcjb rdzx';                               // SMTP password
+            $mail->SMTPSecure = 'tls';       // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom('your_email@gmail.com', 'University of Kratie');
+            $mail->addAddress($email);     // Add a recipient
+
+            $forgot_password_code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'), 0, 10);
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'UKT Password Reset';
+            $mail->Body    = 'To reset your password click <a href="https://sinhssci-games.com/ukt2.0/pages/adminukt/change_password.php?forgot_password_code=' . $forgot_password_code . '">here </a>. </br>Reset your password in a day.';
+
+
+
+            if ($conn->connect_error) {
+                echo '<p class="text-danger">Could not connect to the database.</p>';
+            } else {
+                $verifyQuery = $conn->query("SELECT * FROM user_account WHERE email = '$email'");
+
+                if ($verifyQuery->num_rows) {
+                    $codeQuery = $conn->query("UPDATE user_account SET forgot_password_code = '$forgot_password_code' WHERE email = '$email'");
+
+                    if ($mail->send()) {
+                        include 'email_success.php';
                     }
+                } else {
+                    include 'error.php';
+                }
+                $conn->close();
+            }
+        } catch (Exception $e) {
+            echo '<img src="../../assets/images/basc.png" alt="Error" />';
+            echo '<h3 class="text-danger text-center">Message could not be sent Please check your internet connection!</h3>';
+        }
+        ?>
 
-                    use PHPMailer\PHPMailer\PHPMailer;
-                    use PHPMailer\PHPMailer\SMTP;
-                    use PHPMailer\PHPMailer\Exception;
-
-                    require '../../mail/Exception.php';
-                    require '../../mail/PHPMailer.php';
-                    require '../../mail/SMTP.php';
-
-                    // Instantiation and passing `true` enables exceptions
-                    $mail = new PHPMailer(true);
-
-                    try {
-                        //Server settings
-                        $mail->isSMTP();                                            // Send using SMTP
-                        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-                        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                        $mail->Username   = 'uktkratie@gmail.com';                     // SMTP username
-                        $mail->Password   = 'fgww ccwv zcjb rdzx';                               // SMTP password
-                        $mail->SMTPSecure = 'tls';       // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-                        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-                        //Recipients
-                        $mail->setFrom('your_email@gmail.com', 'University of Kratie');
-                        $mail->addAddress($email);     // Add a recipient
-
-                        $forgot_password_code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'), 0, 10);
-
-                        // Content
-                        $mail->isHTML(true);
-                        $mail->Subject = 'UKT Password Reset';
-                        $mail->Body    = 'To reset your password click <a href="https://sinhssci-games.com/ukt2.0/pages/adminukt/change_password.php?forgot_password_code=' . $forgot_password_code. '">here </a>. </br>Reset your password in a day.';
-
-                        $conn = new mysqli('localhost', 'u123573546_uktadmin', 'UKT2.0_db', 'u123573546_uktnew_db');
-
-                        if ($conn->connect_error) {
-                            echo '<p class="text-danger">Could not connect to the database.</p>';
-                        } else {
-                            $verifyQuery = $conn->query("SELECT * FROM user_account WHERE email = '$email'");
-
-                            if ($verifyQuery->num_rows) {
-                                $codeQuery = $conn->query("UPDATE user_account SET forgot_password_code = '$forgot_password_code' WHERE email = '$email'");
-                            
-                                if ($mail->send()) {
-                                    include 'email_success.php';
-                                } 
-                            } else {
-                                include 'error.php';
-                            }
-                            $conn->close();
-                        }
-                    } catch (Exception $e) {
-                        echo '<img src="../../assets/images/basc.png" alt="Error" />';
-                        echo '<h3 class="text-danger text-center">Message could not be sent Please check your internet connection!</h3>';
-                    }
-                    ?>
-    
     </div>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
